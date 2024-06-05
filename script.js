580f0326835d5a28106266aab470c7ce54942622
OmarBaRaean <s202044700@kfupm.edu.sa>
2024-06-05 19:02:55 +0300

    added the skill and contact pages. also made the mobile css

--- script.js
+++ script.js
@@ -8,15 +8,31 @@ const hamOnOff = function () {
   mlinks.classList.toggle("on");
 };
 
-hamicon.addEventListener("click", hamOnOff);
-hamlink.addEventListener("click", hamOnOff);
-
-// this function will make the header get smaller when you scroll down the page and return it to its original size when you are at the top
-window.addEventListener("scroll", () => {
+const headerAdjust = () => {
   const header = document.querySelector("header");
   if (window.scrollY === 0) {
     header.style.height = "15vh";
   } else {
     header.style.height = "8vh";
   }
+};
+
+hamicon.addEventListener("click", hamOnOff);
+hamlink.addEventListener("click", hamOnOff);
+
+// this function will make the header get smaller when you scroll down the page and return it to its original size when you are at the top
+window.addEventListener("scroll", headerAdjust);
+
+document.addEventListener("DOMContentLoaded", function () {
+  headerAdjust();
+  const skills = document.querySelectorAll(".skill");
+
+  skills.forEach(function (element) {
+    element.addEventListener("mouseenter", () => {
+      element.classList.toggle("skill-hover");
+    });
+    element.addEventListener("mouseleave", () => {
+      element.classList.toggle("skill-hover");
+    });
+  });
 });
